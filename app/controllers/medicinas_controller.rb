dbb01a60de49827655439535d55a20e7f3fb5d0b
masakazematsumoto <kazecocoa0919@gmail.com>
2020-11-03 15:29:13 +0900

    index,new修正

--- app/controllers/medicinas_controller.rb
+++ app/controllers/medicinas_controller.rb
@@ -5,7 +5,19 @@ class MedicinasController < ApplicationController
   end
 
   def new
-    @medicinas = Medicina.new
+    @medicina= Medicina.new
   end
   
+  def create
+    @medicina = Medicina.create(medicina_params)
+    if @medicina.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def medicina_params
+    params.require(:medicina).permit(:medicina1, :medicina2, :medicina3, :medicina4, :medicina5, :medicina6, :medicina7, :medicina8, :medicina9 )
+  end
 end
--- app/models/medicina.rb
+++ app/models/medicina.rb
@@ -1,2 +1,13 @@
 class Medicina < ApplicationRecord
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  validates :medicina1
+  validates :medicina2
+  validates :medicina3
+  validates :medicina4
+  validates :medicina5
+  validates :medicina6
+  validates :medicina7
+  validates :medicina8
+  validates :medicina9
+  end
 end

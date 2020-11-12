5a49956e4ca67f07d0b6c54e58358363beefcf31
masakazematsumoto <kazecocoa0919@gmail.com>
2020-11-12 15:50:29 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'medicinas#index'
-  resources :medicinas, only: [:index, :new, :create] 
+  resources :medicinas, only: [:index, :new, :create,] 
+  resources :users, only: [:edit, :update]
 end
